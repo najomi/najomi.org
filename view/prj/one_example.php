125f1a515f7204435ea21765f8a12948a059c14d
Aleksej Kamynin <zendzirou@gmail.com>
2021-04-20 08:51:31 +0300

    комментарии для сайта

--- view/prj/one_example.php
+++ view/prj/one_example.php
@@ -23,3 +23,16 @@
 	 <?=$data->prop('author')?>
 </div>
 <? endif ?>
+
+<div id='discourse-comments'></div>
+
+<script type="text/javascript">
+  DiscourseEmbed = { discourseUrl: 'https://forum.bubujka.org/',
+  discourseEmbedUrl: 'https://najomi.org/<?=path()?>' };
+
+  (function() {
+    var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
+    d.src = DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
+  })();
+</script>
