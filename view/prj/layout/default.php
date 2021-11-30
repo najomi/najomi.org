da9d7c71758ffb74daed3c7cfdcd80a40dd1eb4e
Aleksej Kamynin <zendzirou@gmail.com>
2021-11-30 17:34:04 +0300

    gtm

--- view/prj/layout/default.php
+++ view/prj/layout/default.php
@@ -10,8 +10,19 @@
     <meta name="keywords" content="<?=(is_array(keywords()) ? implode(', ', keywords()) : keywords())?>">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <script src="https://yastatic.net/pcode/adfox/loader.js" crossorigin="anonymous"></script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K3WH5R9');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3WH5R9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
   <div class="container">
 
     <div class="col-md-10 col-md-push-1">
